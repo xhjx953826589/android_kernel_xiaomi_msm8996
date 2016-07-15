8b3794cc62c657221b4943eae880b8e4f4daed7d
Sultanxda <sultanxda@gmail.com>
2016-07-15 09:52:27 -0700

    power: Add back OnePlus fingerprintd() speed hack
    Signed-off-by: dd3boh <dade.garberi@gmail.com>
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -45,6 +45,10 @@ static bool suspend_freeze_wake;
 extern int PROC_AWAKE_ID; /* 12th bit */
 extern int slst_gpio_base_id;
 
+#ifdef CONFIG_MACH_MSM8996_15801
+extern void thaw_fingerprintd(void);
+#endif
+
 void freeze_set_ops(const struct platform_freeze_ops *ops)
 {
 	lock_system_sleep();
@@ -362,6 +366,9 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 
  Platform_wake:
 	platform_resume_noirq(state);
+#ifdef CONFIG_MACH_MSM8996_15801
+	thaw_fingerprintd();
+#endif
 	dpm_resume_noirq(PMSG_RESUME);
 
  Platform_early_resume:
