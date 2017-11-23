61f50ddf54abe25b42677276f120bdca345215b0
jonascardoso <jonascard60@gmail.com>
2017-11-23 11:28:05 -0200

    build: new script

---
+++ zip-creator/base/anykernel.sh
@@ -0,0 +1,38 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=FloppyKernel by JonasCardoso @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=gemini
+device.name2=Gemini
+device.name3=MI5
+device.name4=mi5
+device.name5=
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## AnyKernel permissions
+# set permissions for included ramdisk files
+chmod -R 755 $ramdisk
+
+## AnyKernel install
+dump_boot;
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
