6820a78c3ae350e8ffcaaaa667f16b9604b42cc5
Jonas Cardoso <jonascard60@gmail.com>
2017-12-26 15:40:42 -0200

    zip-creator: update AnyKernel2
    Signed-off-by: Jonas Cardoso <jonascard60@gmail.com>

--- zip-creator/base/anykernel.sh
+++ zip-creator/base/anykernel.sh
@@ -2,28 +2,36 @@
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
-# EDIFY properties
-do.devicecheck=0
-do.initd=0
-do.cleanup=1
-do.cleanuponabort=1
+# begin properties
+properties() {
+kernel.string=
+do.devicecheck=
+do.modules=
+do.cleanup=
+do.cleanuponabort=
 device.name1=
 device.name2=
 device.name3=
 device.name4=
 device.name5=
+} # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
+ramdisk_compression=auto;
+
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
 
-## AnyKernel permissions
-# set permissions for included ramdisk files
-chmod -R 755 $ramdisk
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
 
 ## AnyKernel install
 dump_boot;
@@ -41,3 +49,4 @@ insert_line init.rc "import /init.spectrum.rc" after "import /init.floppy.rc" "i
 write_boot;
 
 ## end install
+
