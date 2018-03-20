dc9d8473f39eecce5955b897c0ad882d89d517fe
xNombre <kartapolska@gmail.com>
2018-03-20 12:57:12 +0100

    Charge control: release ver 2.1
    Version 2.0 was only an unfinished piece of crap. Now let's fix it finally.

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -49,6 +49,7 @@ static int fb_notifier_callback(struct notifier_block *self,
 
 #ifdef CONFIG_CHARGE_CONTROL
 #include "charge_control.h"
+static bool recharge_able = 0;
 #endif
 
 /* Mask/Bit helpers */
@@ -503,11 +504,7 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-#ifdef CONFIG_CHARGE_CONTROL
-#define smbchg_default_hvdcp3_icl_ma maximum_qc_current
-#else
 static int smbchg_default_hvdcp3_icl_ma = 2700;
-#endif
 
 module_param_named(
 	default_hvdcp3_icl_ma, smbchg_default_hvdcp3_icl_ma,
@@ -1061,11 +1058,11 @@ static int get_prop_batt_status(struct smbchg_chip *chip)
 	}
 
 	if (reg & BAT_TCC_REACHED_BIT)
-		return POWER_SUPPLY_STATUS_FULL;
+		goto battery_full;
 
 	chg_inhibit = reg & CHG_INHIBIT_BIT;
 	if (chg_inhibit)
-		return POWER_SUPPLY_STATUS_FULL;
+		goto battery_full;
 
 	rc = smbchg_read(chip, &reg, chip->chgr_base + CHGR_STS, 1);
 	if (rc < 0) {
@@ -1091,6 +1088,13 @@ static int get_prop_batt_status(struct smbchg_chip *chip)
 out:
 	pr_smb_rt(PR_MISC, "CHGR_STS = 0x%02x\n", reg);
 	return status;
+
+battery_full:
+	if(trigger_full_charge){
+		vote(chip->battchg_suspend_votable, BATTCHG_USER_EN_VOTER, 1, 0);
+		finish_full_charge();
+	}
+	return POWER_SUPPLY_STATUS_FULL;
 }
 
 #define BAT_PRES_STATUS			0x08
@@ -1220,20 +1224,19 @@ static int get_prop_batt_capacity(struct smbchg_chip *chip)
 
 
 #ifdef CONFIG_CHARGE_CONTROL
-	if(trigger_full_charge) {
-		if(get_prop_batt_status(chip) == POWER_SUPPLY_STATUS_FULL) {
-			vote(chip->battchg_suspend_votable, BATTCHG_USER_EN_VOTER, 1, 0);
-			finish_full_charge();
+	if(!trigger_full_charge) {
+		if(unlikely(recharge_able && capacity <= recharge_at)) {
+			recharge_able = 0;
+			vote(chip->battchg_suspend_votable, BATTCHG_USER_EN_VOTER, 0, 0);
 		}
-	}
-	else if(unlikely(recharge_at && get_prop_batt_status(chip) == POWER_SUPPLY_STATUS_NOT_CHARGING && capacity <= recharge_at)) {
-		vote(chip->battchg_suspend_votable, BATTCHG_USER_EN_VOTER, 0, 0);
-	}
-	else if(unlikely(capacity >= charge_limit && get_prop_batt_status(chip) == POWER_SUPPLY_STATUS_CHARGING)) {
-		vote(chip->battchg_suspend_votable, BATTCHG_USER_EN_VOTER, 1, 0);
+		else if(unlikely(charge_limit && capacity >= charge_limit && get_prop_batt_status(chip) == POWER_SUPPLY_STATUS_CHARGING)) {
+			vote(chip->battchg_suspend_votable, BATTCHG_USER_EN_VOTER, 1, 0);
 
-		if(full_charge_every)
-			count_charge();
+			if(full_charge_every)
+				count_charge();
+
+			recharge_able = 1;
+		}
 	}
 #endif
 
@@ -2743,8 +2746,7 @@ static int set_fastchg_current_vote_cb(struct device *dev,
 	return 0;
 }
 
-#if 0
-static int smbchg_set_fastchg_current_user(struct smbchg_chip *chip,
+int smbchg_set_fastchg_current_user(struct smbchg_chip *chip,
 							int current_ma)
 {
 	int rc = 0;
@@ -2756,7 +2758,6 @@ static int smbchg_set_fastchg_current_user(struct smbchg_chip *chip,
 		pr_err("Couldn't vote en rc %d\n", rc);
 	return rc;
 }
-#endif
 
 static struct ilim_entry *smbchg_wipower_find_entry(struct smbchg_chip *chip,
 				struct ilim_map *map, int uv)
@@ -3928,6 +3929,16 @@ static int smbchg_config_chg_battery_type(struct smbchg_chip *chip)
 	 * Only configure from profile if fastchg-ma is not defined in the
 	 * charger device node.
 	 */
+#ifdef CONFIG_CHARGE_CONTROL
+	rc = vote(chip->fcc_votable, BATT_TYPE_FCC_VOTER, true,
+					maximum_qc_current);
+	if (rc < 0) {
+		dev_err(chip->dev,
+			"Couldn't vote for fastchg current rc=%d\n",
+			rc);
+		return rc;
+	}
+#else
 	if (!of_find_property(chip->spmi->dev.of_node,
 				"qcom,fastchg-current-ma", NULL)) {
 		rc = of_property_read_u32(profile_node,
@@ -3948,6 +3959,7 @@ static int smbchg_config_chg_battery_type(struct smbchg_chip *chip)
 			}
 		}
 	}
+#endif
 
 	return ret;
 }
@@ -8583,6 +8595,10 @@ static int smbchg_probe(struct spmi_device *spmi)
 			smbchg_hvdcp_enable_cb);
 	if (IS_ERR(chip->hvdcp_enable_votable))
 		return PTR_ERR(chip->hvdcp_enable_votable);
+
+#ifdef CONFIG_CHARGE_CONTROL
+	chip_pointer = chip;
+#endif
 
 	INIT_WORK(&chip->usb_set_online_work, smbchg_usb_update_online_work);
 	INIT_WORK(&chip->late_init_work, smbchg_late_init_work);
